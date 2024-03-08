bd762ea1fae065f1c020586d222caf085c4e5b02
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-03-08 18:19:09 -0500

    Support excel download

--- postprocess/excel.py
+++ postprocess/excel.py
@@ -11,6 +11,13 @@ def write_excel_json(dist_mix_pair_dict, dir_path):
     folder_name = os.path.basename(os.path.normpath(dir_path))
     excel_file_path = os.path.join(output_dir, f"{folder_name}_pairs.xlsx")
     json_file_path = os.path.join(output_dir, f"{folder_name}_pairs.json")
+
+    category_mapping = {
+        1: "deficiency",
+        2: "full_occupancy_atomic_mixing",
+        3: "deficiency_no_atomic_mixing",
+        4: "full_occupancy"
+    }
     
     with pd.ExcelWriter(excel_file_path, engine='openpyxl') as excel_writer:
         for pair, files_info in dist_mix_pair_dict.items():
@@ -20,16 +27,19 @@ def write_excel_json(dist_mix_pair_dict, dir_path):
             df.rename(columns={
                 'index': 'File',
                 'dist': 'Distance',
-                'mixing': 'Mixing'
+                'mixing': 'Atomic Mixing'
             }, inplace=True)
             
             df['Distance'] = pd.to_numeric(
                 df['Distance'], errors='coerce').astype(float)
-            df['Mixing'] = pd.to_numeric(
-                df['Mixing'], errors='coerce').astype(int)
+             
+            df['Atomic Mixing'] = df['Atomic Mixing'].apply(pd.to_numeric, errors='coerce').map(category_mapping).fillna("Unknown category")
             df['File'] = df['File'].apply(lambda x: f"{x}.cif")
             df.sort_values(by='Distance', inplace=True)
             
+            # Specify the desired column order
+            df = df[['File', 'Distance', 'Atomic Mixing']]
+
             # Write DataFrame to Excel
             sheet_name = f"{pair[:31]}"  # Excel sheet name limit
             df.to_excel(excel_writer, sheet_name=sheet_name, index=False)
